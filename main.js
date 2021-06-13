a22f7bdbbc849810674b8ea1dd29879b2aba9bfa
K. Russell Smith <velocirussell@gmail.com>
2021-06-13 15:12:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 'use strict';
 /*
- a basic, no-frills snake game
- — K. Russell Smith
+ a basic, no-frills snake game, by K. Russell Smith
 */
 import { Timer, text } from './aux.js';
+
 const Dir = {
 	LEFT:   [-1, +0],
 	RIGHT:  [+1, +0],
 	UP:     [+0, -1],
 	DOWN:   [+0, +1],
 };
-// Precomputed:
 Dir.inverse = (() =>
 {
 	const compleMap = (...pairs) =>
@@ -30,17 +29,16 @@ Dir.inverse = (() =>
 
 class Snake
 {
-	constructor(x, y, grid)
+	constructor(x, y)
 	{
 		this.dir = Dir.RIGHT;
 		this.nodes = [[x, y]];
-		this.grid  = grid;
 	}
 	grow()
 	{
 		const [ x, y ] = this.nodes[0];
 		const [ growX, growY ] = this.dir;
-		this.nodes.splice(0, 0, [x + growX, y + growY]);
+		this.nodes.unshift([x + growX, y + growY]);
 	}
 	move()
 	{
@@ -106,6 +104,7 @@ class Snake
 		return this.nodes.length;
 	}
 }
+
 const Game = ({
 	snakePos = [0, 0],
 	tick     = 100,
@@ -113,16 +112,24 @@ const Game = ({
 	size     = 10}) =>
 ({
 	tick,
-	snake: null,
 	fish: [0, 0],
 	storageKey: 'KRS-snake-score',
 	init()
 	{
-		this.snake = new Snake(snakePos[0], snakePos[1]),
-		this.spawnFish();
-		this.score     = 0;
+		this.snake     = new Snake(snakePos[0], snakePos[1]),
+		this.points    = 0;
 		this.highScore = localStorage.getItem(this.storageKey) ?? 0;
+		this.spawnFish();
 		return this;
+	},
+	score(points)
+	{
+		this.points += points;
+		if (this.points >= this.highScore)
+		{
+			this.highScore = this.points;
+			localStorage.setItem(this.storageKey, this.highScore);
+		}
 	},
 	update()
 	{
@@ -133,12 +140,7 @@ const Game = ({
 		{
 			snake.grow();
 			this.spawnFish();
-			++this.score;
-			if (this.score >= this.highScore)
-			{
-				this.highScore = this.score;
-				localStorage.setItem(this.storageKey, this.highScore);
-			}
+			this.score(1);
 		}
 		else
 		{
@@ -159,14 +161,18 @@ const Game = ({
 			ctx.fillRect(node[0] * cellSize, node[1] * cellSize, cellSize, cellSize);
 		}
 		ctx.fillStyle = '#20C8F0';
-		ctx.fillRect(fish[0] * cellSize, fish[1] * cellSize, cellSize, cellSize);
+		ctx.fillRect(
+			fish[0] * cellSize, fish[1] * cellSize,
+			cellSize, cellSize);
 		ctx.strokeStyle = '#F08080';
-		ctx.lineWidth   = 2;
-		ctx.strokeRect(0, 0, size * cellSize, size * cellSize);
+		ctx.lineWidth   = width / 200;
+		ctx.strokeRect(
+			ctx.lineWidth / 2, ctx.lineWidth / 2,
+			size * cellSize - ctx.lineWidth, size * cellSize - ctx.lineWidth);
 		ctx.restore();
 		ctx.textAlign = 'left';
 		ctx.textBaseline = 'top';
-		text(ctx, `${this.score}\n${this.highScore}`, width * 0.025, height * 0.025, {
+		text(ctx, `${this.points}\n${this.highScore}`, width * 0.025, height * 0.025, {
 			font: `bold ${(width / 20) | 0}px monospace`,
 			spacing: width / 40,
 		}, '#20C8F0');
@@ -226,14 +232,34 @@ export default function main()
 		cellSize,
 		size,
 	};
-	let game = null;
+	let game = Game(config);
 	const startGame = () =>
 	{
-		game = Game(config).init();
+		game.init();
 		window.requestAnimationFrame(draw);
 	};
-	let time = 0;
-	const timer = Timer();
+	
+	const draw = (() =>
+	{
+		let time = 0;
+		const timer = Timer();
+		return function draw(mil)
+		{
+			timer.update(mil);
+			if (!game.over)
+			{
+				window.requestAnimationFrame(draw);
+			}
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			game.draw(ctx);
+			time += timer.tick;
+			if (time >= game.tick)
+			{
+				game.update();
+				time = 0;
+			}
+		};
+	})();
 	
 	const MC = new Hammer.Manager(canvas);
 	MC.add(new Hammer.Swipe());
@@ -257,21 +283,6 @@ export default function main()
 			startGame();
 		}
 	});
-	function draw(mil)
-	{
-		timer.update(mil);
-		if (!game.over)
-		{
-			window.requestAnimationFrame(draw);
-		}
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		game.draw(ctx);
-		time += timer.tick;
-		if (time >= game.tick)
-		{
-			game.update();
-			time = 0;
-		}
-	}
+	
 	startGame();
 }
